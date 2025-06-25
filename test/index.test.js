308a7ac02222b973c4038e9989f7cdfc3ac33311
prisvega <pmvega@pichincha.com>
2025-06-25 12:28:32 -0500

    update para verificar los cambios del azure

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 
 describe('FastPay CI/CD', () => {
   it('debería ser una prueba exitosa', () => {
+    console.log('FastPay CI/CD test is running');
     assert.strictEqual(1 + 1, 2);
   });
 });
