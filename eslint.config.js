88fc97f14c17aea310cee626dca06b05f22ff408
prisvega <pmvega@pichincha.com>
2025-06-24 19:38:00 -0500

    Agrega ESLint como dependencia de desarrollo y configuración básica

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+export default [
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+      rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+      },
+    },
+  ];
+  
