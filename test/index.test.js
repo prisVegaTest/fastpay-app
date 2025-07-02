846aa13488a611bab88a1371d8dc23dcdeaf219d
prisvega <pmvega@pichincha.com>
2025-07-01 23:36:18 -0500

    agregar prueba rapida

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,13 @@
 const assert = require('assert');
+const request = require('supertest');
 
-describe('FastPay CI/CD', () => {
-  it('debería ser una prueba exitosa', () => {
-    console.log('FastPay CI/CD test is running');
-    assert.strictEqual(1 + 1, 2);
+const API_URL = 'https://fastpay-app-ezckema9bmgqagcf.canadacentral-01.azurewebsites.net'; 
+
+describe('Home page FastPay API', () => {
+  it('debería responder 200 en /', async () => {
+    const res = await request(API_URL).get('/');
+    assert.strictEqual(res.status, 200);
+    // Ajusta el texto esperado según lo que muestra tu página
+    assert.ok(res.text.includes('FastPay') || res.text.length > 0);
   });
 });
