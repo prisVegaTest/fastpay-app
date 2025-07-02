2c83ac93514cae7f5b8ae7efbfda4fb9f3dcbc98
prisvega <pmvega@pichincha.com>
2025-07-01 23:43:44 -0500

    prettier

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 const request = require('supertest');
 
-const API_URL = 'https://fastpay-app-ezckema9bmgqagcf.canadacentral-01.azurewebsites.net'; 
+const API_URL = 'https://fastpay-app-ezckema9bmgqagcf.canadacentral-01.azurewebsites.net';
 
 describe('Home page FastPay API', () => {
   it('debería responder 200 en /', async () => {
     const res = await request(API_URL).get('/');
     assert.strictEqual(res.status, 200);
-    // resultado esperado 
+    // resultado esperado
     assert.ok(res.text.includes('FastPay') || res.text.length > 0);
   });
 });
